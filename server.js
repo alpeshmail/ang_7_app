b01d34d59114c60cfe99b676ad31a84f652eab9f
alpeshmail@gmail.com <alpeshmail@gmail.com>
2019-02-16 16:41:34 -0500

    test

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ http.createServer(function (req, res) {
   res.end('Node app is up...' + '\n');
 }).listen(port);
 console.log('Server running at port: ' + port);
+/*
+const project = require('./config/project.config')
+const server = require('./server/main')
+const debug = require('debug')('app:bin:server')
+
+server.listen(project.server_port)
+debug(`${project.env} Server is now running at http://localhost:${project.server_port}.`)
+*/
